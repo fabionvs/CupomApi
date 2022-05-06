47d5bb27d0e37129eb4f58095c1c2aafedc69605
fabio.garcia <fabio.garcia@dpu.def.br>
2022-05-06 03:25:01 -0300

    Correção url da autenticação

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -56,7 +56,7 @@ class AuthController extends Controller
         Passport::personalAccessTokensExpireIn($token_time);
         $token = $user->createToken('MyApp')->accessToken;
         //return response()->json(compact('token', 'user', 'token_time'));
-        return Redirect::to('cupomapp://?token='.$token);
+        return Redirect::to('cupomapp://token='.$token);
     }
 
     public function logout()
