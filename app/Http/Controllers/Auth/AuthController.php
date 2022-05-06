3bf02b4fcbc8b3021d4e375d7fe6579ebbecddd4
fabio.garcia <fabio.garcia@dpu.def.br>
2022-05-06 11:59:38 -0300

    Testando url de redirect

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -56,7 +56,7 @@ class AuthController extends Controller
         Passport::personalAccessTokensExpireIn($token_time);
         $token = $user->createToken('MyApp')->accessToken;
         //return response()->json(compact('token', 'user', 'token_time'));
-        return Redirect::to('cupomapp://token='.$token);
+        return Redirect::to('exp://192.168.15.2:19000?token='.$token);
     }
 
     public function logout()
