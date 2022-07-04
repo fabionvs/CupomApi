5499e3754f6be36ec6b5ea4df0426234df2c129e
fabio.garcia <fabio.garcia@dpu.def.br>
2022-07-03 23:53:15 -0300

    corrigindo upload :(

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -94,7 +94,6 @@ class AuthController extends Controller
         $image = $request->image;  // your base64 encoded
         $image = str_replace('data:image/png;base64,', '', $image);
         $image = str_replace(' ', '+', $image);
-        $imageName = str_random(10) . '.png';
 
         $imageName = $user->id.'.'.'png';
         file_put_contents(public_path(). "/images/" . $imageName, base64_decode($image));
